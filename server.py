6588fd38da1fe3719bb340d800c5203e57bb28af
PC002 <singhpriyansu09@gmail.com>
2026-08-06 18:33:17 +0530

    fix

--- server.py
+++ server.py
@@ -113,4 +113,5 @@ async def api_summarize(file: UploadFile = File(...), summary_length: str = Form
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=True)
